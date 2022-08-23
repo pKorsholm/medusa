52267d500726ef83b3d3ef6d6b7a6cad2cd9caf4
Philip Korsholm <philip.korsholm@hotmail.com>
2022-08-23 15:15:56 +0700

    switch to cobertura format

--- packages/medusa/jest.config.js
+++ packages/medusa/jest.config.js
@@ -19,6 +19,8 @@ module.exports = {
     [`json`, { file: "report.json" }],
     `html`,
     `lcov`,
+    'text', 
+    'cobertura'
   ],
   transform: {
     "^.+\\.[jt]s?$": `../../jest-transformer.js`,
